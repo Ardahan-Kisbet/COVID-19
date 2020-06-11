3abcd7e2acc40ebbfc82f64e54dec8ac22fc55e8
Ardahan Kisbet <ardahankisbet@gmail.com>
2020-06-11 20:46:48 +0300

    unit test for github api usage has been added

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { configure, shallow, render } from "enzyme";
-import chai, { expect } from "chai";
+import chai, { expect, should } from "chai";
 //import TestComponent from "../Components/TestComponent";
-import App from "../App";
+//import App from "../App";
 import chaiEnzyme from "chai-enzyme";
 
 import Adapter from "enzyme-adapter-react-16";
@@ -21,4 +21,19 @@ describe("App Component testing", function () {
   });
 
   chai.use(chaiEnzyme());
+});
+
+// Checking githubData.js
+const githubData = require("../githubData.js");
+it("Should return the total repo count related with COVID on github", async () => {
+  // no done
+  const data = await githubData.getTotalRepoCount();
+  expect(data).greaterThan(10000);
+  expect(data).to.be.a("number");
+});
+
+it("Should return array of repo counts by monthly", async () => {
+  // no done
+  const data = await githubData.getRepoCounts();
+  expect(data).to.be.a("array", "Not an array!");
 });
